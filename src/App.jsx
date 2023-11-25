dac366550864008b52be0d61bbc9a59c83dc2c0e
JMCA2805 <josemauricio2805@gmail.com>
2023-11-24 23:38:03 -0400

    Creando la estructura base del FrontEnd
    Close #1

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AuthProvider from "./contexts/AuthProvider";
+import DarkModeGlobal from "./contexts/DarkModeProvider";
+import Home from "./components/Home"
+function App() {
+  return (
+    <>
+      <AuthProvider>
+        <DarkModeGlobal>
+          <BrowserRouter>
+            {/* <Header /> */}
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+            {/* <Footer /> */}
+          </BrowserRouter>
+        </DarkModeGlobal>
+      </AuthProvider>
+    </>
+  );
+}
+
+export default App;
