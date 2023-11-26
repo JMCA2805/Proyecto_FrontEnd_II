67f473aba3116d08b971b447b858bb501a573eb9
CrimvaelVR <maikelvr2004@gmail.com>
2023-11-26 11:08:13 -0400

    UPDATE / Perfil completamente actualizable
    El perfil peude ahora actualizar los datos de su descripción, telefono e imagen, del usuario activo

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,20 +1,23 @@
 import React, { useEffect, useState, useContext } from "react";
 import { AuthContext } from "../contexts/AuthProvider";
 import axios from "axios";
-import Swal from 'sweetalert2'; // Importar SweetAlert2
+import Swal from "sweetalert2"; // Importar SweetAlert2
 
 const API = import.meta.env.VITE_USER_URL;
 const API2 = import.meta.env.VITE_USERS_URL;
+const APIEDIT = import.meta.env.VITE_EDIT_URL;
+
 
 const UserProfile = () => {
   const { user } = useContext(AuthContext);
   const [userData, setUserData] = useState(null);
   const [mostrarMensaje, setMostrarMensaje] = useState(false);
   const [imagen, setImagen] = useState("");
-  const [respuesta, setRespuesta] = useState('');
-  const [mostrarFormulario, setMostrarFormulario] = useState(false); 
+  const [respuesta, setRespuesta] = useState("");
+  const [mostrarFormulario, setMostrarFormulario] = useState(false);
+  const [datosActualizados, setDatosActualizados] = useState({});
+
   const fetchUserData = async () => {
-    
     try {
       const response = await axios.get(`${API}/${user.id}`);
       setUserData(response.data);
@@ -29,50 +32,49 @@ const UserProfile = () => {
     }
   }, [user]);
 
-
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
     const formData = new FormData();
-    formData.append('imagen', imagen);
-  
+    formData.append("imagen", imagen);
+
     // Verificar que los campos obligatorios no estén vacíos
     if (!imagen) {
-      setRespuesta('Por favor, completa todos los campos.');
+      setRespuesta("Por favor, completa todos los campos.");
       setMostrarMensaje(true);
       return;
     }
-  
+
     try {
       const response = await axios.post(`${API2}/${user.id}`, formData, {
         headers: {
-          'Content-Type': 'multipart/form-data',
+          "Content-Type": "multipart/form-data",
         },
       });
-      setMostrarFormulario(false)
+      setMostrarFormulario(false);
       setUserData(response.data);
       setMostrarMensaje(true);
       fetchUserData();
-  
+
       setTimeout(() => {
         setMostrarMensaje(false);
       }, 3000);
-  
+
       // Mostrar mensaje de confirmación con SweetAlert2
       Swal.fire({
-        icon: 'success',
-        title: 'Foto de perfil cambiada',
-        text: 'Foto de perfil cambiada exitosamente.',
+        icon: "success",
+        title: "Foto de perfil cambiada",
+        text: "Foto de perfil cambiada exitosamente.",
       });
     } catch (error) {
       console.error(error);
-      setRespuesta('Ocurrió un error al cambiar la foto de perfil');
-  
+      setRespuesta("Ocurrió un error al cambiar la foto de perfil");
+
       // Mostrar mensaje de error con SweetAlert2
       Swal.fire({
-        icon: 'error',
-        title: 'Error',
-        text: 'Ocurrió un error al cambiar la foto de perfil',
+        icon: "error",
+        title: "Error",
+        text: "Ocurrió un error al cambiar la foto de perfil",
       });
     }
   };
@@ -81,26 +83,31 @@ const UserProfile = () => {
     setMostrarFormulario(!mostrarFormulario);
   };
 
-  const actualizarUsuario = (event) => {
-    event.preventDefault(); // Evita que la página se reinicie por defecto
+  //ACTUALIZAR DATOS
 
+  const editarUsuario = (usuario) => {
+    setDatosActualizados({ ...usuario });
+  };
+  
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setDatosActualizados({ ...datosActualizados, [name]: value });
+  };
+  const actualizarUsuario = (e) => {
+    e.preventDefault(); // Evita que la página se reinicie por defecto
+    console.log(datosActualizados)
+
+  
     axios
-      .put(APIEDIT, {
-        correo: usuarioSeleccionado.correo,
+      .post(`${APIEDIT}/${user.id}`, {
+        id: user.id,
         datosActualizados,
       })
       .then((response) => {
-        const usuariosActualizados = users.map((usuario) => {
-          if (usuario.correo === usuarioSeleccionado.correo) {
-            return { ...usuario, ...datosActualizados };
-          }
-          return usuario;
-        });
-        setUsers(usuariosActualizados);
-
-        setUsuarioSeleccionado(null);
+        setUserData({ ...userData, ...datosActualizados });
+  
         setDatosActualizados({});
-
+  
         // Mensaje de confirmación
         Swal.fire({
           icon: "success",
@@ -111,7 +118,7 @@ const UserProfile = () => {
       })
       .catch((error) => {
         console.error("Error al actualizar el usuario:", error);
-
+  
         // Mensaje de error
         Swal.fire({
           icon: "error",
@@ -120,7 +127,6 @@ const UserProfile = () => {
         });
       });
   };
-  
 
   ///Esto estara proximamente integrado con nuestra Base de Datos de Usuarios, para detectar el token actual y compararlo con los usuarios para asi encontrar sus datos
   return (
@@ -129,7 +135,6 @@ const UserProfile = () => {
         <div className="dark:bg-azulO/80 bg-azulC shadow overflow-hidden sm:rounded-lg border border-azulO dark:border-azulC">
           {/* Banner */}
           <div className="bg-azulC dark:bg-azulO border-b border-azulO dark:border-azulC px-4 py-5 sm:px-6">
-  
             <div className="flex justify-between">
               <div className="flex items-center">
                 <img
@@ -141,17 +146,16 @@ const UserProfile = () => {
                   {`${userData?.nombre} ${userData?.apellido}`}
                 </h1>
               </div>
-  
+
               {/* Botón "Cambiar Foto" */}
               <button
                 className="py-2 px-4 rounded-md col-span-2 border-b-4 dark:border-azulC border-azulO hover:bg-azulO/50 dark:hover:bg-azulC/70 focus-within:bg-azulO text-white bg-azulO/30 dark:bg-azulC"
                 onClick={handleClickCambiarFoto}
               >
-                {mostrarFormulario ? "Ocultar Formulario" : "Cambiar Foto"}
+                {mostrarFormulario ? "Ocultar Formulario" : "Editar Perfil"}
               </button>
             </div>
-  
-  
+
             {/* Formulario de carga de imagen */}
             {mostrarFormulario && (
               <form
@@ -168,24 +172,23 @@ const UserProfile = () => {
                   }}
                   name="imagen"
                 />
-  
+
                 <button
                   type="submit"
-                  className="py-2 px-4 rounded-md w-full col-span-2  border-b-4 dark:border-azulC border-azulO hover:bg-azulO/50 dark:hover:bg-azulC/70 focus-within:bg-azulO text-white bg-azulO/30 dark:bg-azulC"
+                  className="py-2 px-4 rounded-md w-full col-span-2  border-b-4 dark:border-azulC border-azulO hover:bg-azulO/50 dark:hover:bg-azulC/70 focus-within:bg-azulO text-white bg-azulO/30 dark:bg-azul"
                 >
                   Enviar Foto
                 </button>
               </form>
             )}
-  
+
             {mostrarMensaje && (
               <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 mt-6 rounded">
                 <p className="text-sm font-bold">{respuesta}</p>
               </div>
             )}
-  
           </div>
-  
+
           {/* User Info */}
           <div className="px-4 py-5 sm:p-6">
             <div className="flex items-center mb-4">
@@ -204,9 +207,7 @@ const UserProfile = () => {
                 />
               </svg>
               <span className="text-white font-bold">Correo electrónico:</span>
-              <span className="ml-2 text-white">
-                {userData?.correo}
-              </span>
+              <span className="ml-2 text-white">{userData?.correo}</span>
             </div>
             <div className="flex items-center mb-4">
               <svg
@@ -224,9 +225,7 @@ const UserProfile = () => {
                 />
               </svg>
               <span className="text-white font-bold">Teléfono:</span>
-              <span className="ml-2 text-white">
-                {userData?.telefono}
-              </span>
+              <span className="ml-2 text-white">{userData?.telefono}</span>
             </div>
             <div className="flex items-center">
               <svg
@@ -244,15 +243,61 @@ const UserProfile = () => {
                 />
               </svg>
               <span className="text-white font-bold">Descripción:</span>
-              <span className="ml-2 text-white">
-                {userData?.descripcion}
-              </span>
+              <span className="ml-2 text-white">{userData?.descripcion}</span>
             </div>
+
+            {mostrarFormulario && (
+    <div className="mx-4 md:mx-24 font-[Barlow] bg-MoradoC dark:bg-MoradoO rounded-lg p-4 text-white mt-8 border dark:border-VerdeC border-MoradoO">
+      <div className="mx-4 md:mx-24  font-[Barlow] bg-MoradoC dark:bg-MoradoO rounded-lg p-4 text-white mt-8 border dark:border-VerdeC border-MoradoO">
+  <h2 className="text-lg font-semibold">Editar usuario</h2>
+  <form>
+    {/* Campos existentes */}
+    <div className="flex flex-col mt-2">
+      <label htmlFor="descripcion">Descripción:</label>
+      <input
+        type="text"
+        id="descripcion"
+        name="descripcion"
+        value={datosActualizados.descripcion || ""}
+        onChange={handleInputChange}
+        className="w-full p-2 border border-verdeo rounded-md bg-MoradoO/30 border-MoradoO text-black placeholder:text-white/50 focus:border-2 focus:border-MoradoO focus:ring-0 dark:border-VerdeC/50 dark:focus:border-VerdeC"
+      />
+    </div>
+    <div className="flex flex-col mt-2">
+      <label htmlFor="telefono">Teléfono:</label>
+      <input
+        type="text"
+        id="telefono"
+        name="telefono"
+        value={datosActualizados.telefono || ""}
+        onChange={handleInputChange}
+        className="w-full p-2 border border-verdeo rounded-md bg-MoradoO/30 border-MoradoO text-black placeholder:text-white/50 focus:border-2 focus:border-MoradoO focus:ring-0 dark:border-VerdeC/50 dark:focus:border-VerdeC"
+      />
+    </div>
+    {/* Otros campos del formulario */}
+    <div className="flex mt-4">
+
+      <button
+        className="text-white px-4 py-2 rounded-lg mr-2 bg-Moradote focus:outline-none focus:text-white border-b-4 dark:border-VerdeC border-MoradoO hover:bg-Moradote/50 dark:hover:bg-MoradoC/70 focus-within:bg-MoradoO"
+        onClick={actualizarUsuario}
+      >
+        Guardar
+      </button>
+    </div>
+  </form>
+</div>
+    </div>
+  )}
+
+            
+
+
+
           </div>
         </div>
       </div>
     </div>
   );
-  };
-  
-  export default UserProfile;
+};
+
+export default UserProfile;
