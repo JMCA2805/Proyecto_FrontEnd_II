ca0906335c5f3c2d1a02d6fb05696635d1b05001
JMCA2805 <josemauricio2805@gmail.com>
2023-11-26 00:34:33 -0400

    Creación de Ofertas y Demandas para la Landing
    #14

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero";
+import Offers from "./components/Offers";
 
 export default function Landing() {
   return (
     <>
       <Hero />
+      <Offers />
     </>
   );
 }
--- src/components/Landing/components/Hero.jsx
+++ src/components/Landing/components/Hero.jsx
@@ -11,11 +11,11 @@ export default function Hero() {
   return (
     <>
       <section className="bg-[url('/Hero.jpg')] bg-cover bg-center dark:bg-gray-900 font-poppins bg-fixed sm:bg-center">
-        <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div className="px-8 py-8 mx-auto lg:py-16">
           {/* Contenido principal */}
-          <div className="mr-auto place-self-center lg:col-span-7">
+          <div className="md:w-1/2 2xl:w-full">
             {/* Título */}
-            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight text-white leading-none md:text-5xl xl:text-7xl dark:text-white">
+            <h1 className="w-2xl 2xl:w-full mb-4 text-4xl font-extrabold tracking-tight text-white leading-none md:text-5xl xl:text-7xl dark:text-white">
               WebStore Wonderland
             </h1>
             {/* Descripción */}
