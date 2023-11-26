0fe482665d6327f6dd52c28d3d5c26bc8d434d26
CrimvaelVR <maikelvr2004@gmail.com>
2023-11-25 20:13:11 -0400

    CREAR / AuthProvider y ProtectedRoute
    Se implemento el authprovider y una ruta de ejemplo para el Admin, protegida por el componente protectedroute

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Home from "./components/Home"
 import Login from "./components/Login"
 import Registro from "./components/Registro"
 
+import ProtectedRoute from "./components/ProtectedRoute";
+
 function App() {
   return (
     <>
@@ -16,6 +18,13 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="/Login" element={<Login />} />
               <Route path="/Registro" element={<Registro />} />
+
+              <Route element={<ProtectedRoute allowedRoles={["admin"]} />}>
+                <Route
+                  path="/Admin"
+                  element={<Home />}
+                />
+              </Route>
 
 
             </Routes>
