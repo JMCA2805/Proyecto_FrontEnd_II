4cbe9c555f0304474d15c989929b6cbc707e2ff3
Luis Monsalve <126512492+Luismonsalver@users.noreply.github.com>
2023-11-26 16:20:24 -0400

    Upload/Arreglando las vistas de las ofertras
    #14

--- src/components/Landing/components/Offers.jsx
+++ src/components/Landing/components/Offers.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 // Api del servidor backend
-const API = import.meta.env.VITE_GETOFFE_URL;
+const API = import.meta.env.VITE_GETOFF_URL;
 
 export default function Offers() {
   // Estados
@@ -18,6 +18,8 @@ export default function Offers() {
       .catch((error) => {
         console.log(error);
       });
+      
+      console.log(ofertas)
   };
   useEffect(() => {
     getoffers();
@@ -40,20 +42,20 @@ export default function Offers() {
           </p>
         </div>
         {ofertas != "" ? (
-          <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 lg:gap-12">
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 lg:gap-12 md:pb-8 pb-4">
             {/* Seccion que muestra y condiciona si mostrar o no os ofertas */}
             {ofertas.map((off, index) => (
-              <div key={index} className="flex flex-col items-center">
-                <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+              <div key={index} className="flex flex-col items-center px-6">
+                <div className="flex justify-center items-center mb-2 w-10 h-10 rounded-full lg:h-12 lg:w-12 ">
                   <img
                     src={off.imagen}
-                    className="w-6 h-6 text-primary-600 lg:w-8 lg:h-8 dark:text-primary-300"
+                    className="w-10 h-10"
                   />
                 </div>
-                <h3 className="mb-2 text-xl font-bold dark:text-white">
-                  {off.off}
+                <h3 className="mb-2 text-xl font-bold dark:text-white text-azulO text-center">
+                  {off.oferta}
                 </h3>
-                <p className="text-white dark:text-gray-400">
+                <p className="text-azulO dark:text-gray-400 text-justify">
                   {off.descripcion}
                 </p>
               </div>
