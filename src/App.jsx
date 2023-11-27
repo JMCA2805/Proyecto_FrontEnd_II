258fa015427f73ae8c2bf3c7fa05ce94cfbd30bb
CrimvaelVR <maikelvr2004@gmail.com>
2023-11-27 15:11:08 -0400

    Update // Ahora las tablas funcionan con botones
    Para mas comodidad, se ve una tabla a la vez en vez de todas a la vez

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useState } from "react";
 import AuthProvider from "./contexts/AuthProvider";
 import DarkModeGlobal from "./contexts/DarkModeProvider";
 import Landing from "./components/Landing/Landing";
@@ -19,6 +20,13 @@ import UserTable from "./components/Admin/TablaUsers";
 import TablaProductos from "./components/Admin/TablaProductos"
 
 function App() {
+
+  const [showTablaArt, setShowTablaArt] = useState(false);
+  const [showTablaOffers, setShowTablaOffers] = useState(false);
+  const [showUserTable, setShowUserTable] = useState(false);
+  const [showTablaProductos, setShowTablaProductos] = useState(false);
+
+  
   return (
     <>
       <AuthProvider>
@@ -38,19 +46,83 @@ function App() {
               <Route path="/Blog" element={<HeroBlog />} />
               <Route path="/Registro" element={<Registro />} />
 
-              <Route element={<ProtectedRoute allowedRoles={["admin"]} />}>
-                <Route
-                  path="/Admin"
-                  element={
-                    <>
-                      <TablaArt />
-                      <TablaOffers />
-                      <UserTable />
-                      <TablaProductos/>
-                                         </>
-                  }
-                />
-              </Route>
+              <Route
+    path="/Admin"
+    element={
+      <div>
+        <div className="flex justify-center mb-4 mt-4">
+          <div className="flex flex-col items-center gap-4">
+            <button
+              className={`block md:inline-block rounded-md p-2 text-white font-bold bg-azul focus:outline-none focus:text-white border-b-4 ${
+                showTablaArt ? 'border-azulO' : 'border-azul'
+              } dark:border-azulO/70 ${
+                showTablaArt ? 'hover:bg-azulC' : 'hover:bg-azul'
+              } focus-within:bg-azulO`}
+              onClick={() => {
+                setShowTablaArt(!showTablaArt);
+                setShowTablaOffers(false);
+                setShowUserTable(false);
+                setShowTablaProductos(false);
+              }}
+            >
+              Mostrar/ocultar TablaArt
+            </button>
+            <button
+              className={`block md:inline-block rounded-md p-2 text-white font-bold bg-azul focus:outline-none focus:text-white border-b-4 ${
+                showTablaOffers ? 'border-azulO' : 'border-azul'
+              } dark:border-azulO/70 ${
+                showTablaOffers ? 'hover:bg-azulC' : 'hover:bg-azul'
+              } focus-within:bg-azulO`}
+              onClick={() => {
+                setShowTablaOffers(!showTablaOffers);
+                setShowTablaArt(false);
+                setShowUserTable(false);
+                setShowTablaProductos(false);
+              }}
+            >
+              Mostrar/ocultar TablaOffers
+            </button>
+            <button
+              className={`block md:inline-block rounded-md p-2 text-white font-bold bg-azul focus:outline-none focus:text-white border-b-4 ${
+                showUserTable ? 'border-azulO' : 'border-azul'
+              } dark:border-azulO/70 ${
+                showUserTable ? 'hover:bg-azulC' : 'hover:bg-azul'
+              } focus-within:bg-azulO`}
+              onClick={() => {
+                setShowUserTable(!showUserTable);
+                setShowTablaArt(false);
+                setShowTablaOffers(false);
+                setShowTablaProductos(false);
+              }}
+            >
+              Mostrar/ocultar UserTable
+            </button>
+            <button
+              className={`block md:inline-block rounded-md p-2 text-white font-bold bg-azul focus:outline-none focus:text-white border-b-4 ${
+                showTablaProductos ? 'border-azulO' : 'border-azul'
+              } dark:border-azulO/70 ${
+                showTablaProductos ? 'hover:bg-azulC' : 'hover:bg-azul'
+              } focus-within:bg-azulO`}
+              onClick={() => {
+                setShowTablaProductos(!showTablaProductos);
+                setShowTablaArt(false);
+                setShowTablaOffers(false);
+                setShowUserTable(false);
+              }}
+            >
+              Mostrar/ocultar TablaProductos
+            </button>
+          </div>
+        </div>
+          <div>
+            {showTablaArt && <TablaArt />}
+            {showTablaOffers && <TablaOffers />}
+            {showUserTable && <UserTable />}
+            {showTablaProductos && <TablaProductos />}
+        </div>
+      </div>
+    }
+  />
               
               <Route
                 element={<ProtectedRoute allowedRoles={["admin", "usuario"]} />}
