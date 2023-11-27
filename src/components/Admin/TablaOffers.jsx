333cc9c1e534442a7153a688123ba78ae15a74ee
Luis Monsalve <126512492+Luismonsalver@users.noreply.github.com>
2023-11-26 21:40:02 -0400

    Upload/Acomodando responsive
    #14

--- src/components/Admin/TablaOffers.jsx
+++ src/components/Admin/TablaOffers.jsx
@@ -103,14 +103,14 @@ const TablaOffers = () => {
         handleUp={handleUp}
       />
       <div className="w-full px-10 h-full">
-        <div className="overflow-x-auto px-8 py-0 w-full h-full rounded-xl dark:bg-azulO/50 bg-azulC/80 mb-4">
-          <div className="font-[Barlow] mb-8">
+        <div className="md:px-8 px-4 py-4 w-full h-full rounded-xl dark:bg-azulO/50 bg-azulC/80 mb-4">
+          <div className="font-[Barlow] mb-8 px-4">
             <div className="bg-azul dark:bg-azulO rounded-lg p-4 mx-4 mt-4 sm:mx-28 mb-2 border dark:border-azulC border-azulO">
               <h2 className="text-white text-3xl font-bold text-center">
                 Lista de Ofertas y Descuentos
               </h2>
             </div>
-            <div className="w-full flex justify-end items-end pb-2">
+            <div className="w-full flex justify-center items-center md:justify-end md:items-end pb-2">
               <button
                 className="block md:inline-block rounded-md p-2 text-white font-bold bg-azul focus:outline-none focus:text-white border-b-4 border-azulO dark:border-azulO/70 hover:bg-azulC focus-within:bg-azulO"
                 onClick={handleModalSet}
@@ -123,31 +123,31 @@ const TablaOffers = () => {
                 <p>No hay ofertas disponibles.</p>
               </div>
             ) : (
-              <div className="inline-block min-w-full shadow rounded-xl overflow-hidden h-full dark:border-azulC border border-azulO">
-                <table className="min-w-full leading-normal text-xs md:text-sm text-left">
+              <div className="overflow-x-auto  shadow rounded-xl overflow-hidden h-full dark:border-azulC border border-azulO">
+                <table className=" w-full leading-normal text-xs md:text-sm text-left">
                   <thead className="text-white bg-azul dark:bg-azulO border-b dark:border-azulC border-azulO">
                     <tr className="text-center">
                       <th
                         scope="col"
-                        className="sm:p-2 md:px-6 md:py-3 text-center"
+                        className="px-4 md:py-3 text-center"
                       >
                         Oferta / Descuento
                       </th>
                       <th
                         scope="col"
-                        className="sm:p-2 md:px-6 md:py-4 whitespace-nowrap overflow-y-auto max-h-40"
+                        className="px-4 md:py-4 whitespace-nowrap overflow-y-auto max-h-40"
                       >
                         Descripción
                       </th>
                       <th
                         scope="col"
-                        className="sm:p-2 md:px-6 md:py-3 text-center"
+                        className="px-4 md:py-3 text-center w-32"
                       >
                         Icono
                       </th>
                       <th
                         scope="col"
-                        className="sm:p-2 md:px-6 md:py-3 text-center"
+                        className="px-4 md:py-3 text-center"
                       >
                         Acciones
                       </th>
@@ -157,20 +157,20 @@ const TablaOffers = () => {
                     {Array.isArray(ofertas) ? (
                       ofertas.map((oferta, index) => (
                         <tr key={index}>
-                          <td className="sm:p-2 md:px-6 md:py-4 whitespace-nowrap">
+                          <td className="px-4 md:py-4 whitespace-nowrap">
                             {oferta.oferta}
                           </td>
-                          <td className="sm:p-2 md:px-6 md:py-4 whitespace-nowrap overflow-y-auto max-h-40">
+                          <td className="px-4 md:py-4 whitespace-nowrap overflow-y-auto max-h-40">
                             {oferta.descripcion}
                           </td>
-                          <td className="sm:p-2 md:px-6 md:py-4 whitespace-nowrap">
+                          <td className="px-4">
                             <img
                               src={oferta.imagen}
                               alt={oferta.oferta}
-                              className="w-20 h-20 object-cover rounded-full mx-auto"
+                              className="w-20 max-w-none object-cover rounded-full"
                             />
                           </td>
-                          <td className="sm:p-2 md:px-6 md:py-4 whitespace-nowrap">
+                          <td className="px-4 md:py-4 whitespace-nowrap">
                             <div className="flex items-center gap-4">
                               <button
                                 className="block md:inline-block rounded-md p-2 text-white font-bold bg-azul focus:outline-none focus:text-white border-b-4 border-azulO dark:border-azulO/70 hover:bg-azulC focus-within:bg-azulO"
