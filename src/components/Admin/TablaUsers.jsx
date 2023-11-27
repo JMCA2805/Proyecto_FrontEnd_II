8f444d6323bf5e5f5bc4a333d7b279b316d522d9
CrimvaelVR <maikelvr2004@gmail.com>
2023-11-26 21:21:40 -0400

    UPDATE / Modal para el Editar Usuario
    Ahora el administrador podrá editar usuario con un modal de forma mas comoda

--- src/components/Admin/TablaUsers.jsx
+++ src/components/Admin/TablaUsers.jsx
@@ -1,15 +1,19 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Swal from "sweetalert2";
+import EditUser from "./Modal/EditUser";
 
 const API = import.meta.env.VITE_USERS_URL;
-const APIEDIT = import.meta.env.VITE_EDIT_URL;
 const APIDELETE = import.meta.env.VITE_ELIMINARUSUARIO_URL;
 
 const UserTable = () => {
   const [users, setUsers] = useState([]);
   const [usuarioSeleccionado, setUsuarioSeleccionado] = useState(null);
   const [datosActualizados, setDatosActualizados] = useState({});
+
+  
+  const [up, setUp] = useState(true);
+  const handleUp = () => setUp(!up);
 
   useEffect(() => {
     // Lógica para obtener los datos de los usuarios desde el backend
@@ -21,7 +25,15 @@ const UserTable = () => {
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  }, [up]);
+
+
+
+
+  const [openModal, setOpenModal] = useState(false);
+  const handleModalSet = () => setOpenModal(!openModal);
+  const [openEditModal, setOpenEditModal] = useState(false);
+  const handleEditModalSet = () => setOpenEditModal(!openEditModal);
 
   const editarUsuario = (usuario) => {
     setUsuarioSeleccionado(usuario);
@@ -32,46 +44,7 @@ const UserTable = () => {
     setDatosActualizados({ ...datosActualizados, [name]: value });
   };
 
-  const actualizarUsuario = (event) => {
-    event.preventDefault(); // Evita que la página se reinicie por defecto
-
-    axios
-      .put(APIEDIT, {
-        correo: usuarioSeleccionado.correo,
-        datosActualizados,
-      })
-      .then((response) => {
-        const usuariosActualizados = users.map((usuario) => {
-          if (usuario.correo === usuarioSeleccionado.correo) {
-            return { ...usuario, ...datosActualizados };
-          }
-          return usuario;
-        });
-        setUsers(usuariosActualizados);
-
-        setUsuarioSeleccionado(null);
-        setDatosActualizados({});
-
-        // Mensaje de confirmación
-        Swal.fire({
-          icon: "success",
-          title: "Usuario actualizado",
-          showConfirmButton: false,
-          timer: 1500,
-        });
-      })
-      .catch((error) => {
-        console.error("Error al actualizar el usuario:", error);
-
-        // Mensaje de error
-        Swal.fire({
-          icon: "error",
-          title: "Error al actualizar el usuario",
-          text: "Ocurrió un error al actualizar los datos del usuario. Por favor, inténtalo nuevamente.",
-        });
-      });
-  };
-
+ 
   const eliminarUsuario = (correo) => {
     // Mostrar confirmación con SweetAlert
     Swal.fire({
@@ -117,7 +90,18 @@ const UserTable = () => {
   };
 
   return (
-    <div className="w-full px-10 h-full">
+    <>  
+    <EditUser
+        openEditModal={openEditModal}
+        handleEditModalSet={handleEditModalSet}
+        usuarioSeleccionado={usuarioSeleccionado}
+        setUsuarioSeleccionado={setUsuarioSeleccionado}
+        setDatosActualizados={setDatosActualizados}
+        handleInputChange={handleInputChange}
+        datosActualizados={datosActualizados}
+        handleUp={handleUp}
+      />
+     <div className="w-full px-10 h-full">
       <div className="overflow-x-auto px-8 py-0 w-full h-full rounded-xl dark:bg-azulO/50 bg-azulC/80 mb-4">
         <div className="font-poppins mb-8">
         <div className="bg-azul dark:bg-azulO rounded-lg p-4 mx-4 mt-4 sm:mx-28 mb-2 border dark:border-azulC border-azulO">
@@ -197,7 +181,11 @@ const UserTable = () => {
                             <div className="flex items-center gap-4">
                                 <button
                                 className="block md:inline-block rounded-md p-2 text-white font-bold bg-azul focus:outline-none focus:text-white border-b-4 border-azulO dark:border-azulO/70 hover:bg-azulC focus-within:bg-azulO"
-                                onClick={() => editarUsuario(usuario)}
+                                onClick={() => {
+                                  editarUsuario(usuario)
+                                  handleEditModalSet();
+
+                                }}
                                 >
                                   Editar
                                 </button>
@@ -227,78 +215,14 @@ const UserTable = () => {
             </div>
           )}
 
-          {usuarioSeleccionado && (
-            <div className="mx-4 md:mx-24  font-[Barlow] bg-MoradoC dark:bg-MoradoO rounded-lg p-4 text-white mt-8 border dark:border-VerdeC border-MoradoO">
-              <h2 className="text-lg font-semibold">Editar usuario</h2>
-              <form>
-                <div className="flex flex-col mt-2">
-                  <label htmlFor="nombre">Nombre:</label>
-                  <input
-                    type="text"
-                    id="nombre"
-                    name="nombre"
-                    value={datosActualizados.nombre || ""}
-                    onChange={handleInputChange}
-                    className="w-full p-2 border border-verdeo rounded-md bg-MoradoO/30 border-MoradoO text-white placeholder:text-white/50 focus:border-2 focus:border-MoradoO focus:ring-0 dark:border-VerdeC/50 dark:focus:border-VerdeC"
-                  />
-                </div>
-                <div className="flex flex-col mt-2">
-                  <label htmlFor="apellido">Apellido:</label>
-                  <input
-                    type="text"
-                    id="apellido"
-                    name="apellido"
-                    value={datosActualizados.apellido || ""}
-                    onChange={handleInputChange}
-                    className="w-full p-2 border border-verdeo rounded-md bg-MoradoO/30 border-MoradoO text-white placeholder:text-white/50 focus:border-2 focus:border-MoradoO focus:ring-0 dark:border-VerdeC/50 dark:focus:border-VerdeC"
-                  />
-                </div>
-                <div className="flex flex-col mt-2">
-                  <label htmlFor="descripcion">Descripcion:</label>
-                  <input
-                    type="text"
-                    id="descripcion"
-                    name="descripcion"
-                    value={datosActualizados.descripcion || ""}
-                    onChange={handleInputChange}
-                    className="w-full p-2 border border-verdeo rounded-md bg-MoradoO/30 border-MoradoO text-white placeholder:text-white/50 focus:border-2 focus:border-MoradoO focus:ring-0 dark:border-VerdeC/50 dark:focus:border-VerdeC"
-                  />
-                </div>
-                <div className="flex flex-col mt-2">
-                  <label htmlFor="telefono">Teléfono:</label>
-                  <input
-                    type="text"
-                    id="telefono"
-                    name="telefono"
-                    value={datosActualizados.telefono || ""}
-                    onChange={handleInputChange}
-                    className="w-full p-2 border border-verdeo rounded-md bg-MoradoO/30 border-MoradoO text-white placeholder:text-white/50 focus:border-2 focus:border-MoradoO focus:ring-0 dark:border-VerdeC/50 dark:focus:border-VerdeC"
-                  />
-                </div>
-                {/* Otros campos del formulario */}
-                <div className="flex mt-4">
-                  <button
-                    type="submit"
-                    className="text-white px-4 py-2 rounded-lg mr-2 bg-Moradote focus:outline-none focus:text-white border-b-4 dark:border-VerdeC border-MoradoO hover:bg-Moradote/50 dark:hover:bg-MoradoC/70 focus-within:bg-MoradoO"
-                    onClick={() => {
-                      setUsuarioSeleccionado(null);
-                    }}
-                  >
-                    Cancelar
-                  </button>
-                  <button
-                    className="text-white px-4 py-2 rounded-lg mr-2 bg-Moradote focus:outline-none focus:text-white border-b-4 dark:border-VerdeC border-MoradoO hover:bg-Moradote/50 dark:hover:bg-MoradoC/70 focus-within:bg-MoradoO"
-                    onClick={actualizarUsuario}
-                  >
-                    Guardar
-                  </button>
-                </div>
-              </form>
-            </div>
-          )}
+      
         </div>
       </div>
-    </div>
+    </div>  
+    </>
+
+
+   
   );
 };
 
